2b8d5cdd07538b03725404b754b92ec5f1f3237a
ambivalent-axiom <artmelnis@gmail.com>
2024-06-12 16:33:00 +0300

    add guzzle, reconfigure to guzzle

--- app/Api/CoinMC.php
+++ app/Api/CoinMC.php
@@ -1,7 +1,10 @@
 <?php
 namespace Ambax\CryptoTrade\Api;
+use GuzzleHttp\Client;
+
 class CoinMC implements Api
 {
+    private CLient $client;
     private string $url;
     private string $params;
     private array $headers;
@@ -9,13 +12,16 @@ class CoinMC implements Api
     {
         if (isset($_ENV['COINMC'])) {
             $this->headers = [
-                'Accepts: application/json',
-                'X-CMC_PRO_API_KEY: ' . $_ENV['COINMC'],
+                'Accepts' => 'application/json',
+                'X-CMC_PRO_API_KEY' => $_ENV['COINMC'],
             ];
         } else {
             throw new \Exception("Fatal Error! Coinmarketcap API key not loaded! Unable to load crypto database.\n");
         }
-        $this->url = 'https://pro-api.coinmarketcap.com/';
+        $this->client = new Client([
+            'base_uri' => 'https://pro-api.coinmarketcap.com/',
+            'timeout'  => 2.0,
+        ]);
         $this->params = http_build_query([
             'start' => $start,
             'limit' => $limit,
@@ -24,16 +30,10 @@ class CoinMC implements Api
     }
     public function get($method): string
     {
-        $url = $this->url . $method . "?" . $this->params;
-        $request = curl_init();
-        curl_setopt($request, CURLOPT_URL, $url);
-        curl_setopt($request, CURLOPT_HTTPHEADER, $this->headers);
-        curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
-        if( ! $result = curl_exec($request))
-        {
-            trigger_error(curl_error($request));
-        }
-        curl_close($request);
-        return $result;
+        $url = $method . "?" . $this->params;
+        $response = $this->client->get($url, [
+            'headers' => $this->headers
+        ]);
+        return $response->getBody()->getContents();
     }
 }
